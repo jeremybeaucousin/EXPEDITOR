8036dfb6b5d721b9b644f371935d41c9192b8259
fperrais2015 <fperrais2015@128-27.ad.campus-eni.fr>
2015-12-21 16:43:30 +0100

    commit stable

--- ExpeditorProject/src/main/java/fr/eni/servlet/Index.java
+++ ExpeditorProject/src/main/java/fr/eni/servlet/Index.java
@@ -1,7 +1,10 @@
 package fr.eni.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,13 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login
  */
-public class Login extends HttpServlet {
+@WebServlet("/")
+public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * Default constructor. 
      */
-    public Login() {
+    public Index() {
         // TODO Auto-generated constructor stub
     }
 
@@ -23,14 +27,14 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 }
